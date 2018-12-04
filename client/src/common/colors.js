03c17f05cb0eeb5f2cefb5f12a147a24a5f35e6a
Antonia Blair <antoniablair@gmail.com>
2018-12-04 17:49:28 -0500

    Remove some colors too

--- client/src/common/colors.js
+++ client/src/common/colors.js
@@ -3,10 +3,5 @@ export default {
   white: '#fff',
   purple: '#413c58',
   blueGrey: '#a3c4bc',
-  // seaFoam: '#bfd7b5',
-  // gold: '#e6af2e',
-  // brightCoral: '#ff5c39',
-  // grey: '#6e6e6e',
-  // turquoise: '#26a69a',
   turquoise: '#6dd3ce',
 };
