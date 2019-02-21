5825f923bbfd62d4fa5d9bd0f752584b3f6b0f66
andres <drejauregui@gmail.com>
2019-02-20 21:10:58 -0500

    Removed a font that I decided not to use this time

--- client/src/common/colors.js
+++ client/src/common/colors.js
@@ -5,5 +5,5 @@ export default {
   purple: '#413c58', //remove?
   riptide: '#9bdbc5', //for question background
   steelBlue: '#4682b4', //for buttons and progress bars; replaces blueGrey #a3c4bc
-  turquoise: '#6dd3ce',
+  turquoise: '#6dd3ce', //for that beachy look
 };
